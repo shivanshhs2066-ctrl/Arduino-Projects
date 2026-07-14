751b0f83f251976d5cc89f865cfd7bde7d5a5b76
shivanshhs2066-ctrl <shivanshhs2066@gmail.com>
2026-07-14 17:23:09 -0400

    Add files via upload

---
+++ 02_Button_LED/button_led/sketch_jul14a.ino
@@ -0,0 +1,15 @@
+const int buttonPin = 2;
+const int ledPin = 13;
+
+void setup() {
+  pinMode(buttonPin, INPUT_PULLUP);
+  pinMode(ledPin, OUTPUT);
+}
+
+void loop() {
+  if (digitalRead(buttonPin) == LOW) {
+    digitalWrite(ledPin, HIGH);
+  } else {
+    digitalWrite(ledPin, LOW);
+  }
+}
