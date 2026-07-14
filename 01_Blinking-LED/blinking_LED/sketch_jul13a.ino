5fddb7f373a342d6a50cb42c73eed6f73f31635f
shivanshhs2066-ctrl <shivanshhs2066@gmail.com>
2026-07-14 15:21:01 -0400

    Add files via upload

---
+++ 01_Blinking-LED/blinking_LED/sketch_jul13a.ino
@@ -0,0 +1,13 @@
+void setup() {
+  //pinmode(13, output); put your setup code here, to run once:
+
+}
+
+void loop() {
+digitalWrite(13, HIGH); //LED ON put your main code here, to run repeatedly:
+delay(2000); //wait 2 second
+
+digitalWrite(13, LOW); //LED OFF
+delay(1000); //Wait one sec
+}
+
